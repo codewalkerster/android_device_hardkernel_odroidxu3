4814d5fd8e5893ee756910740b6bb75cf01d267a
codewalker <codewalker@hardkernel.com>
2014-08-21 16:25:22 +0900

    Fix to copy idc files.
    Change-Id: Ic58ac21472a2cfd9a913332e66830a46e4e2c7c4

--- device.mk
+++ device.mk
@@ -159,8 +159,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/hardkernel/proprietary/bin/Vendor_03fc_Product_05d8.idc:system/usr/idc/Vendor_03fc_Product_05d8.idc \
     device/hardkernel/proprietary/bin/Vendor_1870_Product_0119.idc:system/usr/idc/Vendor_1870_Product_0119.idc \
-    device/hardkernel/proprietary/bin/Vendor_1870_Product_0119.idc:system/usr/idc/Vendor_1870_Product_0100.idc \
-    device/hardkernel/proprietary/bin/Vendor_1870_Product_0119.idc:system/usr/idc/Vendor_2808_Product_81c9.idc
+    device/hardkernel/proprietary/bin/Vendor_1870_Product_0100.idc:system/usr/idc/Vendor_1870_Product_0100.idc \
+    device/hardkernel/proprietary/bin/Vendor_2808_Product_81c9.idc:system/usr/idc/Vendor_2808_Product_81c9.idc
 
 # XBox 360 Controller kl keymaps
 PRODUCT_COPY_FILES += \
