d143d2b9fca48be1ba2d278fa08e5273d4d54a47
codewalker <codewalker@hardkernel.com>
2015-04-17 14:58:35 +0900

    ODROID-XU3: Delete to copy proprietries.
    Change-Id: I5565f4b4c9c3bcc23512f5824870f05d31ddbc38

--- device.mk
+++ device.mk
@@ -149,47 +149,6 @@ PRODUCT_COPY_FILES += \
     device/hardkernel/odroidxu3/drivers/snd-usb-audio.ko:system/lib/modules/snd-usb-audio.ko \
     device/hardkernel/odroidxu3/drivers/snd-usbmidi-lib.ko:system/lib/modules/snd-usbmidi-lib.ko
 
-
-# Input device calibration files
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/bin/odroid-ts.idc:system/usr/idc/odroid-ts.idc \
-    device/hardkernel/proprietary/bin/odroid-ts.kl:system/usr/keylayout/odroid-ts.kl \
-    device/hardkernel/proprietary/bin/odroid-ts.kcm:system/usr/keylayout/odroid-ts.kcm \
-    device/hardkernel/proprietary/bin/odroid-keypad.kl:system/usr/keylayout/odroid-keypad.kl \
-    device/hardkernel/proprietary/bin/odroid-keypad.kcm:system/usr/keychars/odroid-keypad.kcm
-
-# for USB HID MULTITOUCH
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/bin/Vendor_03fc_Product_05d8.idc:system/usr/idc/Vendor_03fc_Product_05d8.idc \
-    device/hardkernel/proprietary/bin/Vendor_1870_Product_0119.idc:system/usr/idc/Vendor_1870_Product_0119.idc \
-    device/hardkernel/proprietary/bin/Vendor_1870_Product_0100.idc:system/usr/idc/Vendor_1870_Product_0100.idc \
-    device/hardkernel/proprietary/bin/Vendor_2808_Product_81c9.idc:system/usr/idc/Vendor_2808_Product_81c9.idc
-
-# XBox 360 Controller kl keymaps
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/bin/Vendor_045e_Product_0291.kl:system/usr/keylayout/Vendor_045e_Product_0291.kl \
-    device/hardkernel/proprietary/bin/Vendor_045e_Product_0719.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl \
-    device/hardkernel/proprietary/bin/Vendor_045e_Product_0719.kcm:system/usr/keychars/Vendor_045e_Product_0719.kcm
-
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/apk/Ultra_Explorer.apk:system/app/Ultra_Explorer.apk \
-    device/hardkernel/proprietary/apk/jackpal.androidterm.apk:system/app/jackpal.androidterm.apk \
-    device/hardkernel/proprietary/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
-
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/apk/DicePlayer.apk:system/app/DicePlayer.apk \
-    device/hardkernel/proprietary/lib/libSoundTouch.so:system/lib/libSoundTouch.so \
-    device/hardkernel/proprietary/lib/libdice_kk.so:system/lib/libdice_kk.so \
-    device/hardkernel/proprietary/lib/libdice_loadlibrary.so:system/lib/libdice_loadlibrary.so \
-    device/hardkernel/proprietary/lib/libdice_software.so:system/lib/libdice_software.so \
-    device/hardkernel/proprietary/lib/libdice_software_kk.so:system/lib/libdice_software_kk.so \
-    device/hardkernel/proprietary/lib/libffmpeg_dice.so:system/lib/libffmpeg_dice.so \
-    device/hardkernel/proprietary/lib/libsonic.so:system/lib/libsonic.so
-
-# init.d support
-PRODUCT_COPY_FILES += \
-    device/hardkernel/proprietary/bin/sysinit:system/bin/sysinit
-
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.opengles.version=131072 \
     ro.sf.lcd_density=160
@@ -210,6 +169,8 @@ PRODUCT_CHARACTERISTICS := tablet
 
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi xxhdpi
+
+$(call inherit-product-if-exists, device/hardkernel/proprietary/proprietary.mk)
 
 # setup dalvik vm configs.
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
