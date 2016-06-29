826ce73e05ac4c0b94077ba3c6b1c4a19af7ae5f
Luke Go <luke.go@hardkernel.com>
2016-06-29 16:14:59 +0900

    ODROID-XU4: Changed the makebootini script timing.
    - Changed makebootini.sh check timing after bootcomplete.
    
    - Removed the while loop.
    
    Change-Id: Icc9ab4b85605a8898b8182ac1530c112abd0e28f

--- makebootini.sh
+++ makebootini.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-sleep 5
-
-while true
-do
-    if [ -e "/storage/sdcard0/boot.ini" ]
-    then
-        break
-    else
-        cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
-    fi
-    sleep 1
-done
+if [ -e "/storage/sdcard0/boot.ini" ]
+then
+    break
+else
+    cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
+fi
