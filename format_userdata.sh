a9ddf601143918274ce761c393ec44775d7f3d51
codewalker <codewalker@hardkernel.com>
2017-08-10 16:36:01 +0900

    ODROID-XU3/4: Fix format_userdata.sh
    Change-Id: Iebd6a8e36cbcc2a8e7374bf14fd997e2726a492e

--- format_userdata.sh
+++ format_userdata.sh
@@ -6,13 +6,12 @@ data=`blkid /dev/block/mmcblk0p3`
 for x in $data;
     do
     case $x in
-        TYPE=\"ext4\") format=false;
-        break
+        TYPE=\"ext4\") format=false;;
     esac
 done
 
 if [ $format == true ]; then
-    echo format userdata
-    mk_e2fs -F -t ext4 /dev/block/mmcblk0p3;
-    reboot;
+    echo "format userdata partition"
+    mk_e2fs -F -t ext4 /dev/block/mmcblk0p3
+    reboot
 fi
