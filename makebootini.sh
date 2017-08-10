2386040db39a15466ee8f128488229b928f7955f
codewalker <codewalker@hardkernel.com>
2017-08-10 16:30:01 +0900

    Revert "ODROID-XU3/4: Fix to manage vfat partition."
    This reverts commit 9eab92c4ca8004c0fcca3069b3cec851bbc8b301.
    
    Change-Id: I09cd79cc81a7393f14b8f8dc4a5c3adffd34fde9

--- makebootini.sh
+++ makebootini.sh
@@ -1,26 +1,8 @@
 #!/bin/sh
 
-format=true
-
-data=`blkid /dev/block/mmcblk0p1`
-for x in $data;
-    do
-    case $x in
-        TYPE=\"vfat\") format=false;
-        break
-    esac
-done
-
-if [ $format == true ]; then
-    echo format userdata
-    mkfs.vfat -F 32 /dev/block/mmcblk0p1;
-    reboot;
-fi
-
-if [ -e "/storage/internal/boot.ini" ]
+if [ -e "/storage/sdcard0/boot.ini" ]
 then
     break
 else
-    cp /system/etc/boot.ini.template /storage/internal/boot.ini;
-    reboot;
+    cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
 fi
