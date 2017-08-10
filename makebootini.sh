9eab92c4ca8004c0fcca3069b3cec851bbc8b301
codewalker <codewalker@hardkernel.com>
2017-08-10 09:51:28 +0900

    ODROID-XU3/4: Fix to manage vfat partition.
    if vfat partition not exists, format and copy boot.ini.
    
    Change-Id: Iaa683344e88e12283fd6e796ac708c1f036b7402

--- makebootini.sh
+++ makebootini.sh
@@ -1,8 +1,26 @@
 #!/bin/sh
 
-if [ -e "/storage/sdcard0/boot.ini" ]
+format=true
+
+data=`blkid /dev/block/mmcblk0p1`
+for x in $data;
+    do
+    case $x in
+        TYPE=\"vfat\") format=false;
+        break
+    esac
+done
+
+if [ $format == true ]; then
+    echo format userdata
+    mkfs.vfat -F 32 /dev/block/mmcblk0p1;
+    reboot;
+fi
+
+if [ -e "/storage/internal/boot.ini" ]
 then
     break
 else
-    cp /system/etc/boot.ini.template /storage/sdcard0/boot.ini
+    cp /system/etc/boot.ini.template /storage/internal/boot.ini;
+    reboot;
 fi
