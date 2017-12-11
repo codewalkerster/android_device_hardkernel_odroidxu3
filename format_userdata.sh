2b128212e66156bf8c093f8522325d5dd9822462
codewalker <codewalker@hardkernel.com>
2017-12-11 14:44:27 +0900

    ODROID-XU3/4: Add userdata partition resize command.
    Change-Id: I69f45dedc082c64b8e15437821b9b70560fa17eb

--- format_userdata.sh
+++ format_userdata.sh
@@ -3,15 +3,34 @@
 format=true
 
 data=`blkid /dev/block/mmcblk0p3`
-for x in $data;
+for x in $data
     do
     case $x in
         TYPE=\"ext4\") format=false;;
     esac
 done
 
+if [ -f /cache/firstboot ]; then
+        echo "format userdata partition"
+        mk_e2fs -F -t ext4 /dev/block/mmcblk0p3
+        rm -rf /cache/firstboot
+        sync
+        reboot
+fi
+
 if [ $format == true ]; then
-    echo "format userdata partition"
-    mk_e2fs -F -t ext4 /dev/block/mmcblk0p3
+    start=`fdisk -l /dev/block/mmcblk0 | grep mmcblk0p3 | awk '{print $2}'`
+    finish=`fdisk -l /dev/block/mmcblk0 | grep sectors | awk '{print $5}'`
+    fdisk /dev/block/mmcblk0 <<EOF
+d
+3
+n
+p
+$start
+$(($finish-1))
+w
+EOF
+    touch /cache/firstboot
+    sync
     reboot
 fi
