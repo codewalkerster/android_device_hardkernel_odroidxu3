71b62294c2f7cdec045025e081ae845b9ec765fc
codewalker <codewalker@hardkernel.com>
2017-12-29 12:05:33 +0900

    ODROID-XU3/4: Support 180 degree rotation.
    Change-Id: Ib254c7e1766bf62905b91e17ee7c4bf0077126ba

--- rotation.sh
+++ rotation.sh
@@ -5,14 +5,23 @@ if [ "$1" == "portrait" ]; then # if portrait
     sed -i s/persist.demo.hdmirotation=landscape/persist.demo.hdmirotation=portrait/g /system/build.prop
     if [ "$2" == "90" ]; then # if degree == 90
         sed -i s/ro.sf.hwrotation=0/ro.sf.hwrotation=90/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=180/ro.sf.hwrotation=90/g /system/build.prop
         sed -i s/ro.sf.hwrotation=270/ro.sf.hwrotation=90/g /system/build.prop
     else # else degree == 270
         sed -i s/ro.sf.hwrotation=0/ro.sf.hwrotation=270/g /system/build.prop
         sed -i s/ro.sf.hwrotation=90/ro.sf.hwrotation=270/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=180/ro.sf.hwrotation=270/g /system/build.prop
     fi
 elif [ "$1" == "landscape" ]; then # else landscape
     sed -i s/persist.demo.hdmirotation=portrait/persist.demo.hdmirotation=landscape/g /system/build.prop
-    sed -i s/ro.sf.hwrotation=90/ro.sf.hwrotation=0/g /system/build.prop
-    sed -i s/ro.sf.hwrotation=270/ro.sf.hwrotation=0/g /system/build.prop
+    if [ "$2" == "0" ]; then # if degree == 0
+        sed -i s/ro.sf.hwrotation=90/ro.sf.hwrotation=0/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=180/ro.sf.hwrotation=0/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=270/ro.sf.hwrotation=0/g /system/build.prop
+    else # else degree == 180
+        sed -i s/ro.sf.hwrotation=0/ro.sf.hwrotation=180/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=180/ro.sf.hwrotation=180/g /system/build.prop
+        sed -i s/ro.sf.hwrotation=270/ro.sf.hwrotation=270/g /system/build.prop
+    fi
 fi
 mount -o ro,remount /system
