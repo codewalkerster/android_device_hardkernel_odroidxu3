8ff512c36e8c1bec0599b8653eb4404bb199d1ad
Chris <codewalker@hardkernel.com>
2021-01-19 10:15:16 +0900

    ODROID-XU3/4: Support RTL8821CU combo device.
    Change-Id: Id93b43a053c57d10317a4910d4b799c06454d270

--- makebootini.sh
+++ makebootini.sh
@@ -5,3 +5,10 @@ if [ -e "/internal/boot.ini" ]; then
 else
     cp /system/etc/boot.ini.template /internal/boot.ini
 fi
+
+if [ -f "/internal/usb_modeswitch.con" ]
+then
+    break
+else
+    cp /system/etc/usb_modeswitch.conf.template /internal/usb_modeswitch.conf
+fi
